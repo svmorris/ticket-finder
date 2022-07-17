4ff0fab93644794a6eaa0f7508fb450297873dd8
svmorris <svmorris@protonmail.com>
2022-07-17 15:17:43 +0200

    Re-wrote the whole thing

--- ticket-finder/__init__.py
+++ ticket-finder/__init__.py
@@ -1,35 +1,87 @@
 """
-A program to monitor ticket reselling websites for new tickets that go live.
+A discord bot that monitors ticket re-sale websites and alerts as soon as something is available.
 """
 
 import os
 import sys
 import time
+import json
+import asyncio
+import sqlite3
 import discord
 import requests
+import threading
+
+TICKETS = {}
+DB_PATH = "storage/db.sqlite"
+HELP = """**Usage:**:
+```
+Start watching a website:    !watch <platform> <url>
+Show watchlist:              !show
+Stop watching a website:     !stop <number>
+```
+**Example:**
+```!watch twickets https://www.twickets.live/event/1535073546361905152```
+"""
 
 
-# As check is called in a loop,
-# this variable keeps track of how
-# many tickets there were last
-# time around.
-TICKETS: dict = {'responseData': []}
+
+client = discord.Client()
+token = os.getenv('TOKEN')
+if token is None:
+    print("No token supplied")
+    sys.exit(-1)
 
 
-def alert_discord(discord_id: str, discord_token: str):
-    """ Function uses the discord api (discord.py) to send a DM to a user """
-
-    # Hack-y thing to avoid using the async loop
-    class Client(discord.Client):
-        async def on_ready(self):
-            client.fetch_user(discord_id).send("website")
-            await self.close()
-
-    client = Client()
-    client.run(discord_token)
+def debug_log(message: str):
+    """ Send a message into the #logs channel """
+    print(message)
 
 
-def check_twicket(event_id: str) -> bool:
+
+class Database:
+    def __init__(self):
+
+        if os.path.isfile(DB_PATH):
+            self.db = sqlite3.connect(DB_PATH)
+            self.cursor = self.db.cursor()
+
+        else:
+            self.db = sqlite3.connect(DB_PATH)
+            self.cursor = self.db.cursor()
+            self.cursor.execute("CREATE TABLE tickets (\
+                        'owner',\
+                        'website',\
+                        'api_url',\
+                        'human_url'\
+                    )")
+            self.db.commit()
+
+    def __del__(self ):
+        """ Close the Database """
+        self.db.close()
+
+
+    def add_row(self, user: str, website: str, api_url: str, human_url: str):
+        """ Add a value to the database """
+        self.cursor.execute("INSERT INTO tickets VALUES (?, ?, ?, ?)", (user, website, api_url, human_url))
+        self.db.commit()
+
+
+    def get_all(self) -> list:
+        """ Get all tickets from the database """
+        self.cursor.execute("SELECT rowid, * FROM tickets")
+        return self.cursor.fetchall()
+
+
+    def delete_row(self, number: int):
+        """ Stop monitoring a ticket """
+        self.cursor.execute("DELETE FROM tickets WHERE rowid=?", number)
+        self.db.commit()
+
+
+
+def check_twicket(api_url: str) -> bool:
     """
         Return True if something changed on the website since last time.
 
@@ -40,23 +92,24 @@ def check_twicket(event_id: str) -> bool:
             are different or not. I don't really think the likelyhood of this happening warrents 
             spending time to fix it.
     """
-    api_url = f"https://www.twickets.live/services/g2/inventory/listings/{event_id}?api_key=83d6ec0c-54bb-4da3-b2a1-f3cb47b984f1"
 
-    last_round = globals()['TICKETS']
+    last_round = globals()['TICKETS'].get(api_url)
+
 
     try:
         data = requests.get(url=api_url).json()
-        globals()['TICKETS'] = data
+        globals()['TICKETS'][api_url] = data
     except Exception as err: #pylint: disable=broad-except
         print(f"Ignoring error: {err}", file=sys.stderr)
         return False
+
 
     # If there are no tickets available
     if len(data['responseData']) == 0:
         return False
 
     # If this is not the first time the function is called
-    if last_round['responseData'] != 0:
+    if last_round != None:
         if len(data['responseData']) > len(last_round['responseData']):
             last_round = data
             return True
@@ -67,40 +120,90 @@ def check_twicket(event_id: str) -> bool:
 
 
 
-
-
-
-def main():
-    if len(sys.argv) != 6:
-        print("I don't think you gave the correct number of argumnets")
-        print("Usage: ")
-        print("\t ticket-finder twicket <event_id> <api_key> discord <discord_id> <discord_token>")
-        sys.exit(-1)
-
-
-    if sys.argv[1] != "twicket":
-        print("Have not implemented anything for this website yet :/")
-        print("Feel free to PR me.")
-        sys.exit(-2)
-
-
-    if sys.argv[3] != "discord":
-        print("Have not implemented any other way to notify you yet :/")
-        print("Feel free to PR me.")
-        sys.exit(-2)
-
-
-    # Start pytermgui UI
-    # ui()
-
+def scanner():
+    """ Scanner checks every 2 seconds for new tickets """
     while True:
-        found: bool = check_twicket(sys.argv[2])
-        print(f"{found}: {TICKETS['responseData']}")
-        if found:
-            os.system("firefox https://www.twickets.live/event/1535073546361905152")
         time.sleep(1)
+        thread_db = Database()
+        events = thread_db.get_all()
+
+        for event in events:
+            found: bool = check_twicket(event[3])
+            debug_log(found)
+
+            # If something changed
+            if found:
+                async def send_message_to_specific_channel(message: str, channel: int):
+                    await client.get_channel(channel).send(message)
+
+                asyncio.run_coroutine_threadsafe(send_message_to_specific_channel(
+                        f'<@{event[1]}> New ticket at {event[-1]}bc',
+                        998195920514596967,
+                    ),client.loop)
+
+
+
+@client.event
+async def on_message(message) -> str:
+    """ Handle messages """
+    if message.author == client.user:
+        return
+
+
+    if message.content.startswith("!help"):
+        return await message.channel.send(HELP)
+
+
+
+    if message.content.startswith("!watch"):
+        command = message.content.split()
+
+        if command[1] == "twickets":
+            base_url = "https://www.twickets.live/event/"
+            if not command[2].startswith(base_url):
+                return await message.channel.send("Something is wrong with your URL")
+
+            try:
+                event_id = int(command[2].strip().strip("https://www.twickets.live/event/"))
+            except ValueError:
+                return await message.channel.send("Something is wrong with that URL")
+
+            database.add_row(
+                    str(message.author.id),
+                    "twickets",
+                    f"https://www.twickets.live/services/g2/inventory/listings/{event_id}?api_key=83d6ec0c-54bb-4da3-b2a1-f3cb47b984f1",
+                    f"https://www.twickets.live/event/{event_id}",
+                )
+            return await message.channel.send(f"Started monitoring event: {event_id}!")
+
+        return await message.channel.send("I have not implemented anything for this website yet :/")
+
+
+
+    if message.content.startswith("!show"):
+        watchlist = database.get_all()
+        res = ""
+        for event in watchlist:
+            res += f"**{event[0]}**: {event[-1]} ({event[1]})\n"
+        return await message.channel.send(res)
+
+
+
+    if message.content.startswith("!stop"):
+        try:
+            event_id = message.content.split()[1]
+        except ValueError:
+            return await message.channel.send("Not a valid number!")
+
+        database.delete_row(event_id)
+        return await message.channel.send(f"Stopped monitoring event: {event_id}!")
 
 
 
 if __name__ == "__main__":
-    main()
+    database = Database()
+
+    x = threading.Thread(target=scanner, args=())
+    x.start()
+
+    client.run(token)
